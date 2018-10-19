9dc677690388f5a17ea4a6f8425f38eed40159cd
ArnaudSchlumberger <arn.schlumberger@gmail.com>
2018-10-19 22:14:12 +0200

    CodeV1

---
+++ MeArmJoy/MeArmJoy.ino
@@ -0,0 +1,75 @@
+#include <Servo.h>
+
+#define potenMid A0
+#define potenLeft A1
+#define potenRight A2
+
+//Variables pour servo MIDDLE
+int AngMid = 0;
+float dAngMid;
+//Variables pour servo LEFT
+int AngLeft = 0;
+float dAngLeft;
+//Variables pour servo RIGHT
+int AngRight = 0;
+float dAngRight;
+
+
+
+float maxSpeed = 5.0; //Valeur d'incrémentation maximale des angles
+
+//Ci-dessous :  Intervalles de valeurs corrects pour les differents servos
+/*
+  claw.write(100); //0-100
+  left.write(90); //90-180
+  right.write(45); //45-130
+  middle.write(0); //0-165
+*/
+
+Servo claw, left, right, middle;
+void setup() {
+  Serial.begin(9600); //Init serie
+  claw.attach(6);  //Init servos
+  left.attach(9);
+  right.attach(10);
+  middle.attach(11);
+
+  claw.write(100); //Init des positions
+  left.write(90);
+  right.write(45);
+  middle.write(0);
+
+  //Init pins
+  //(Inutile pour des entrées analogiques...)
+  pinMode(potenMid, INPUT);//Init potenMid pour lire tension en sortie du potentiometre 1
+  pinMode(potenLeft, INPUT); //Init potentiometre 2
+  pinMode(potenRight, INPUT); //Init potentio 3
+
+}
+
+
+void loop() {
+  //////MIDDLE
+  //dAngMid = Variation de l'angle AngMid sur une boucle
+  dAngMid = analogRead(potenMid); //Lecture potentiometre 1
+  dAngMid = map(dAngMid, 0, 1023, -maxSpeed, maxSpeed); //mappage : (0,1023)->(-maxspeed,maxspeed)
+  AngMid = AngMid + dAngMid; //Mise a jour de AngMid
+  AngMid = constrain(AngMid, 0, 165);//AngMid limité aux valeurs comprises entre 0 et 165
+  middle.write(AngMid);
+
+  //////LEFT
+  dAngLeft = analogRead(potenLeft);  //Meme fonctionnement que ci-dessus
+  dAngLeft = map(dAngLeft, 0, 1023, -maxSpeed, maxSpeed);
+  AngLeft = AngLeft + dAngLeft;
+  AngLeft = constrain(AngLeft, 90, 180); //Limitation: 90,180
+  left.write(AngLeft);
+
+  //RIGHT
+  dAngRight = analogRead(potenRight);
+  dAngRight = map(dAngRight, 0, 1023, -maxSpeed, maxSpeed);
+  AngRight = AngRight + dAngRight;
+  AngRight = constrain(AngRight, 45, 130); //Limitation : 45, 130
+  right.write(AngRight);
+  delay(100); //Delai pour permettre aux servos de suivre la commande
+}
+
